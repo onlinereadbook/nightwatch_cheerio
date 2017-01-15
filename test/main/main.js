3bf72807b2e5f301d6275d1fc687a60f8665c150
polo13999 <polo13999@gmail.com>
2017-01-15 22:57:26 +0800

    加入爬蟲需要的套件 並建立nightwatch 設定檔

---
+++ test/main/main.js
@@ -0,0 +1,12 @@
+var cheerio = require("cheerio")
+var path = require("path")
+var fs = require("fs")
+var moment = require("moment")
+//var firebaselib = require("./firebaselib")
+module.exports = {
+    '抓取Ipeen的資料': (browser) => {
+        //         // console.log(browser);
+
+
+    }
+}
