c25cf2c5b844f8dc5bf6fe8a3cf3190a08b83039
polo13999 <polo13999@gmail.com>
2017-01-15 23:46:36 +0800

    要用動態給值使用前端ajax時使用

--- test/main/main.js
+++ test/main/main.js
@@ -5,8 +5,18 @@ var moment = require("moment")
 //var firebaselib = require("./firebaselib")
 module.exports = {
     '抓取Ipeen的資料': (browser) => {
-        //         // console.log(browser);
+        browser.
+            url('http://www.ipeen.com.tw/search/taiwan/000/0-100-0-0/')
+            .waitForElementVisible('body', 1000)
+            //.pause(2000)
+            .source(function (result) { // .source() will dump the target page into text format
+                // console.log(result.value);
+                var $ = cheerio.load(result.value) // so it needs to be parse
+                var data = $('.a_search_location').find('category').text();
+                console.log(data);
 
+            })
+            .pause(2000)
 
     }
 }
